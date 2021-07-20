5d97073a5e1806a56cbe7845709f4ea5c26b8e76
Oliver Beckstein (via Travis CI) <orbeckst@gmail.com>
2021-07-20 14:48:45 +0000

    Deploy MDAnalysisData to github.com/MDAnalysis/MDAnalysisData.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.0+5.g56f9ae0',
+    VERSION: '0.8.0+9.g2de91ab',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
