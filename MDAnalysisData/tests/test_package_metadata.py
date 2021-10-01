3a50dbf92262deae7a91d818025c356b630bacd4
Oliver Beckstein <orbeckst@gmail.com>
2021-10-01 11:44:23 -0700

    fix version test for Python 2.7

--- MDAnalysisData/tests/test_package_metadata.py
+++ MDAnalysisData/tests/test_package_metadata.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import six
 from six.moves import reload_module
 import pytest
 
@@ -28,4 +29,4 @@ def test_default_authors(monkeypatch):
 def test_version():
     # very generic because versioneer will provide different strings depending
     # on the repository status
-    assert isinstance(MDAnalysisData.__version__, str)
+    assert isinstance(MDAnalysisData.__version__, six.string_types)
